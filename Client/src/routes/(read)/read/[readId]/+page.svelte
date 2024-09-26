719f224cd05b7004f1923b7226dd543133c6bbf3
sudhanv09 <mystogun125@gmail.com>
2024-09-26 15:35:06 +0800

    refactor the read page

--- Client/src/routes/(read)/read/[readId]/+page.svelte
+++ Client/src/routes/(read)/read/[readId]/+page.svelte
@@ -1,58 +1,50 @@
 <script lang="ts">
-	import type { PageData } from './$types';
-	import { page } from '$app/stores';
-	import {nextChapter} from "$lib/ReadKeybindUtils";
-	import {goto} from "$app/navigation";
-	import "$src/app.postcss";
-	import {getModalStore, getDrawerStore, type DrawerSettings, type ModalSettings} from "@skeletonlabs/skeleton";
+  import type { PageData } from "./$types";
+  import { page } from "$app/stores";
+  import { goto } from "$app/navigation";
+  import "$src/app.postcss";
 
-	export let data: PageData;
-	let scrollContent : HTMLDivElement;
-	const modalStore = getModalStore();
-	const drawerStore = getDrawerStore();
 
-	const url = $page.url.pathname;
-	const id = url.split('/').pop();
+  export let data: PageData;
+  let scrollContent: HTMLDivElement;
 
-	async function handleKeyPress(event: KeyboardEvent) {
-		switch (event.key){
-			case "n":
-				const response = await nextChapter(`http://localhost:5050/read/next?id=${id}`);
-				return (data.content = response);
-			case " ":
-				scrollContent.scroll({left: scrollContent.scrollLeft+scrollContent.clientWidth-20, behavior: 'smooth'});
-				break;
-			case "q":
-				goto("/");
-				break;
-			case "h":
-				const modal: ModalSettings = {
-					type: "component",
-					component: "keybindModal"
-				}
-				modalStore.trigger(modal);
-				break;
-			case "t":
-				const drawer: DrawerSettings = {
-					id: "toc-drawer",
-					width: "w-[480px]",
-					meta: {data: data.toc}
-				}	
-				drawerStore.open(drawer);
-				break;
-		}
-	}
+  const url = $page.url.pathname;
+  const id = url.split("/").pop();
+
+  async function handleKeyPress(event: KeyboardEvent) {
+    switch (event.key) {
+      case "n":
+        const response = await fetch(
+          `http://localhost:5050/read/next?id=${id}`
+        );
+        return (data.content = await response.text());
+      case " ":
+        scrollContent.scroll({
+          left: scrollContent.scrollLeft + scrollContent.clientWidth + 70,
+          behavior: "smooth",
+        });
+        break;
+      case "q":
+        goto("/");
+        break;
+    }
+  }
 </script>
 
 <svelte:window on:keydown={handleKeyPress} />
 
 <svelte:head>
-	<link rel="stylesheet" href={`http://localhost:5050/read/book-css?id=${id}`} />
+  <link
+    rel="stylesheet"
+    href={`http://localhost:5050/read/book-css?id=${id}`}
+  />
 </svelte:head>
 
 <div
-	bind:this={scrollContent}
-	class="columns-3 col-fill-auto w-full h-screen p-4 overflow-x-scroll"
+  bind:this={scrollContent}
+  class="columns-2 gap-12 py-6 h-dvh overflow-x-scroll"
 >
-	{@html data.content}
+<div class="p-8">
+  {@html data.content}
+</div>
 </div>
