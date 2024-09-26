32819dcd90f69b68105e8df9ab1fb006d8789df1
sudhanv09 <mystogun125@gmail.com>
2024-09-26 15:34:45 +0800

    add the book page back

---
+++ Client/src/routes/(main)/[bookId]/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	export let data: PageData;
+
+	const description =
+		data.book.description !== null
+			? data.book.description.replace(/<[^>]*>/g, '')
+			: 'No description';
+</script>
+
+<div class="p-8 mt-8 flex">
+	<img
+		src={`data:image/jpeg;base64,${data.book.cover}`}
+		alt="cover"
+		class="w-48 h-128 object-cover"
+	/>
+
+	<div class="space-y-8 ml-12">
+		<h1 class="text-4xl font-bold">{data.book.title}</h1>
+		<h2 class="text-xl font-semibold">{data.book.author}</h2>
+
+		<p>
+			{description}
+		</p>
+		<button class="btn variant-filled"><a href="/read/{data.book.id}">Read</a></button>
+		<button class="btn variant-filled mx-4">Edit</button>
+	</div>
+</div>
