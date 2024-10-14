fc05684cda4f953a30e00206158a35df9a76efc7
sudhanv09 <mystogun125@gmail.com>
2024-10-14 17:25:52 +0800

    refactor help dialog

---
+++ Client/src/lib/components/HelpModal.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import * as Dialog from "$src/lib/shad/ui/dialog";
+
+    export let modalOpen: boolean;
+</script>
+
+<Dialog.Root open={modalOpen}>
+    <Dialog.Trigger />
+    <Dialog.Content class="sm:max-w-[425px]">
+      <Dialog.Header>
+        <Dialog.Title>Keyboard Shortcuts</Dialog.Title>
+      </Dialog.Header>
+      <div class="card h-fit flex flex-col items-center justify-center p-8">
+        <h2 class="text-xl font-bold">Quick Shortcuts</h2>
+        <ul class="space-y-4 mt-4">
+          <li>
+            <p><kbd>n</kbd>: Next chapter</p>
+          </li>
+          <li>
+            <p><kbd>p</kbd>: Previous chapter</p>
+          </li>
+          <li>
+            <p><kbd>Space</kbd>: Scroll page</p>
+          </li>
+          <li>
+            <p><kbd>q</kbd>: Quit to menu</p>
+          </li>
+          <li>
+            <p><kbd>h</kbd>: Help</p>
+          </li>
+        </ul>
+      </div>
+    </Dialog.Content>
+  </Dialog.Root>
--- Client/src/routes/(read)/read/[readId]/+page.svelte
+++ Client/src/routes/(read)/read/[readId]/+page.svelte
@@ -2,8 +2,8 @@
   import type { PageData } from "./$types";
   import { page } from "$app/stores";
   import { goto } from "$app/navigation";
-  import * as Dialog from "$src/lib/shad/ui/dialog";
   import "$src/app.postcss";
+  import HelpModal from "$src/lib/components/HelpModal.svelte";
 
   export let data: PageData;
   let scrollContent: HTMLDivElement;
@@ -16,12 +16,12 @@
     switch (event.key) {
       case "n": {
         const next = await fetch(`http://localhost:5050/read/next?id=${id}`);
-        data.content = await next.text()
+        data.content = await next.text();
         return data;
       }
       case "p": {
         const prev = await fetch(`http://localhost:5050/read/prev?id=${id}`);
-        data.content = await prev.text()
+        data.content = await prev.text();
         return data;
       }
       case " ":
@@ -41,7 +41,7 @@
 </script>
 
 <svelte:window on:keydown={handleKeyPress} />
-
+<HelpModal {modalOpen} />
 <svelte:head>
   <link
     rel="stylesheet"
@@ -49,38 +49,9 @@
   />
 </svelte:head>
 
-<Dialog.Root open={modalOpen}>
-  <Dialog.Trigger />
-  <Dialog.Content class="sm:max-w-[425px]">
-    <Dialog.Header>
-      <Dialog.Title>Keyboard Shortcuts</Dialog.Title>
-    </Dialog.Header>
-    <div class="card h-fit flex flex-col items-center justify-center p-8">
-      <h2 class="text-xl font-bold">Quick Shortcuts</h2>
-      <ul class="space-y-4 mt-4">
-        <li>
-          <p><kbd>n</kbd>: Next chapter</p>
-        </li>
-        <li>
-          <p><kbd>p</kbd>: Previous chapter</p>
-        </li>
-        <li>
-          <p><kbd>Space</kbd>: Scroll page</p>
-        </li>
-        <li>
-          <p><kbd>q</kbd>: Quit to menu</p>
-        </li>
-        <li>
-          <p><kbd>h</kbd>: Help</p>
-        </li>
-      </ul>
-    </div>
-  </Dialog.Content>
-</Dialog.Root>
-
 <div
   bind:this={scrollContent}
-  class="columns-2 gap-12 py-6 h-dvh overflow-x-scroll"
+  class="columns-2 gap-12 py-6 h-[calc(100dvh-58px)] overflow-x-scroll"
 >
   <div class="p-8">
     {@html data.content}
