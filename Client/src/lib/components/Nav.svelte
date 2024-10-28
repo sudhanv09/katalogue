ca5b61871da73a49790fd340ae66342961bfad4d
sudhanv09 <mystogun125@gmail.com>
2024-10-28 11:57:58 +0800

    use the new shadcn sidebar

--- Client/src/lib/components/Nav.svelte
+++ Client/src/lib/components/Nav.svelte
@@ -1,46 +1,75 @@
-<script lang="ts">
-	import Button from "../shad/ui/button/button.svelte";
-	import { page } from "$app/stores";
-	import { cn } from "../utils";
-	import { cubicInOut } from "svelte/easing";
-	import { crossfade } from "svelte/transition";
-
-	const [send, receive] = crossfade({
-		duration: 250,
-		easing: cubicInOut,
-	});
-	const className: string | undefined | null = undefined;
-	export let items: { href: string; title: string }[];
+<script lang="ts" module>
+	const data = {
+		navMain: [
+			{
+				title: "Library",
+				items: [
+					{
+						title: "Home",
+						url: "/",
+						isActive: true,
+					},
+					{
+						title: "To-Read",
+						url: "/to-read",
+					},
+					{
+						title: "Finished",
+						url: "/finished",
+					},
+					{
+						title: "Authors",
+						url: "/author",
+					},
+				],
+			},
+		],
+	};
 </script>
 
-<div
-	class={cn(
-		"flex space-x-2 flex-col lg:space-x-0 lg:space-y-1",
-		className,
-	)}
->
-	{#each items as item}
-		{@const isActive = $page.url.pathname === item.href}
+<script lang="ts">
+	import * as Sidebar from "$lib/shad/ui/sidebar";
+	import type { ComponentProps } from "svelte";
+  import SearchForm from "./SearchForm.svelte";
 
-		<Button
-			href={item.href}
-			variant="ghost"
-			class={cn(
-				!isActive && "hover:underline",
-				"relative justify-start hover:bg-transparent",
-			)}
-			data-sveltekit-noscroll
-		>
-			{#if isActive}
-				<div
-					class="bg-muted absolute inset-0 rounded-md"
-					in:send={{ key: "active-sidebar-tab" }}
-					out:receive={{ key: "active-sidebar-tab" }}
-				/>
-			{/if}
-			<div class="relative">
-				{item.title}
-			</div>
-		</Button>
-	{/each}
-</div>
+	let { ref = $bindable(null), ...restProps }: ComponentProps<typeof Sidebar.Root> = $props();
+	
+</script>
+
+<Sidebar.Root {...restProps} bind:ref>
+	<Sidebar.Header class="m-4 ">
+		<h1 class="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0 text-center">Katalogue</h1>
+		<SearchForm />
+	</Sidebar.Header>
+	<Sidebar.Content>
+		<Sidebar.Group>
+			<Sidebar.Menu>
+				{#each data.navMain as groupItem (groupItem.title)}
+					<Sidebar.MenuItem>
+						<Sidebar.MenuButton class="text-sm text-neutral-400">
+							{#snippet child({ props })}
+								<h1 {...props}>
+									{groupItem.title}
+								</h1>
+							{/snippet}
+						</Sidebar.MenuButton>
+						{#if groupItem.items?.length}
+							<Sidebar.MenuSub>
+								{#each groupItem.items as item (item.title)}
+									<Sidebar.MenuSubItem>
+										<Sidebar.MenuSubButton isActive={item.isActive}>
+											{#snippet child({ props })}
+												<a href={item.url} {...props}>{item.title}</a>
+											{/snippet}
+										</Sidebar.MenuSubButton>
+									</Sidebar.MenuSubItem>
+								{/each}
+							</Sidebar.MenuSub>
+						{/if}
+					</Sidebar.MenuItem>
+				{/each}
+			</Sidebar.Menu>
+		</Sidebar.Group>
+	</Sidebar.Content>
+	<Sidebar.Rail />
+</Sidebar.Root>
--- Client/src/routes/(main)/+layout.svelte
+++ Client/src/routes/(main)/+layout.svelte
@@ -1,110 +1,17 @@
 <script lang="ts">
   import "$src/app.postcss";
   import Nav from "$lib/components/Nav.svelte";
-  import FileUpload from "$lib/components/FileUpload.svelte";
-  import { Separator } from "$lib/shad/ui/separator";
-
   import { Toaster } from "$lib/shad/ui/sonner";
-  import * as Sheet from "$lib/shad/ui/sheet";
-  import { Button } from "$lib/shad/ui/button";
-  import { Menu, Search } from "lucide-svelte";
-  import { Input } from "$lib/shad/ui/input";
+  import * as Sidebar from "$src/lib/shad/ui/sidebar";
 
   let { data, children } = $props();
-
-  const sidebarNavItems = [
-    {
-      title: "Home",
-      href: "/",
-    },
-    {
-      title: "To-Read",
-      href: "/to-read",
-    },
-    {
-      title: "Finished",
-      href: "/finished",
-    },
-    {
-      title: "Authors",
-      href: "/authors",
-    },
-  ];
 </script>
 
 <Toaster />
-
-<header
-  class="min-h-screen flex flex-col lg:flex-row lg:space-x-12 lg:space-y-16"
->
-  <nav class="hidden lg:block space-y-10 lg:w-[300px] mx-6 my-12 text-center">
-    <h2
-      class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
-    >
-      Katalogue
-    </h2>
-    <FileUpload />
-    <Nav items={sidebarNavItems} />
-    <Separator />
-
-    <div class="space-y-6">
-      <h3 class="scroll-m-20 text-xl font-semibold tracking-tight">Recent</h3>
-      <div class="space-y-3">
-        {#each data.recentBook as item}
-          <li class="list-none text-neutral-500">
-            <a href={`/book/${item.id}`}>{item.title}</a>
-          </li>
-        {/each}
-      </div>
-    </div>
-  </nav>
-  <Sheet.Root>
-    <Sheet.Trigger asChild let:builder>
-      <Button
-        variant="outline"
-        size="icon"
-        class="shrink-0 lg:hidden mt-4"
-        builders={[builder]}
-      >
-        <Menu class="h-5 w-5" />
-        <span class="sr-only">Toggle navigation menu</span>
-      </Button>
-    </Sheet.Trigger>
-    <Sheet.Content side="left" class="flex flex-col">
-      <nav class="space-y-10 lg:w-[300px] mx-6 my-12 text-center">
-        <h2
-          class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
-        >
-          Katalogue
-        </h2>
-        <FileUpload />
-        <Nav items={sidebarNavItems} />
-        <Separator />
-
-        <div class="space-y-6">
-          <h3 class="scroll-m-20 text-xl font-semibold tracking-tight">
-            Recent
-          </h3>
-          <div class="space-y-3">
-            {#each data.recentBook as item}
-              <li class="list-none text-neutral-500">
-                <a href={`/book/${item.id}`}>{item.title}</a>
-              </li>
-            {/each}
-          </div>
-        </div>
-      </nav>
-    </Sheet.Content>
-  </Sheet.Root>
-  <div class="lg:h-dvh w-full mt-4 space-y-8">
-    <div class="relative ml-auto flex-1 md:grow-0">
-      <Search class="text-muted-foreground absolute left-2.5 top-2.5 h-4 w-4" />
-      <Input
-        type="search"
-        placeholder="Search..."
-        class="bg-background w-full rounded-lg pl-8 md:w-[200px] lg:w-[336px]"
-      />
-    </div>
+<Sidebar.Provider>
+  <Nav />
+  <Sidebar.Inset>
+    <Sidebar.Trigger />
     {@render children()}
-  </div>
-</header>
+  </Sidebar.Inset>
+</Sidebar.Provider>
