4bfcfab8324a8d6d8114935e6e2f11fef36c9d44
sudhanv09 <mystogun125@gmail.com>
2025-08-01 19:29:46 +0800

    book info page responsive

--- src/routes/(app)/books/[id]/+page.svelte
+++ src/routes/(app)/books/[id]/+page.svelte
@@ -5,8 +5,8 @@
   let { data }: PageProps = $props();
 </script>
 
-<main class="p-24 flex space-x-36">
-  <img src={data.book.cover} alt="" class="" />
+<main class="p-24 grid sm:grid-cols-1 lg:grid-cols-2 space-x-36">
+  <img src={data.book.cover} alt="" class="sm:h-200px lg:h-400px" />
   <div class="p-4 space-y-6">
     <h1 class="scroll-m-20 text-3xl font-semibold tracking-tight">
       {data.book.title}
@@ -16,7 +16,7 @@
     </h3>
     <p class="text-muted-foreground text-sm">{data.book.description}</p>
 
-    <p class="text-muted-foreground text-md">Progress: {data.book.progress}%</p>
+    <p class="text-md">Progress: {data.book.progress}%</p>
     <div>
       <Button href={`/read/${data.book.id}`}>Read</Button>
     </div>
