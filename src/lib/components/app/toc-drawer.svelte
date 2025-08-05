3e7c968060080fc470fba20a9d3b12c0546c537d
sudhanv09 <mystogun125@gmail.com>
2025-08-05 09:46:35 +0800

    drawer on the left, correct url

--- src/lib/components/app/toc-drawer.svelte
+++ src/lib/components/app/toc-drawer.svelte
@@ -5,11 +5,13 @@
   let {
     id,
     toc_data,
-  }: { id: string|undefined; toc_data: { id: string; title: string; href: string }[] } =
-    $props();
+  }: {
+    id: string | undefined;
+    toc_data: { id: string; title: string; href: string }[];
+  } = $props();
 </script>
 
-<Drawer.Root>
+<Drawer.Root direction="left">
   <Drawer.Trigger><Menu /></Drawer.Trigger>
   <Drawer.Content>
     <Drawer.Header>
@@ -17,7 +19,7 @@
     </Drawer.Header>
     <div class="flex flex-col space-y-4">
       {#each toc_data as item}
-        <a href={`/api/book/${id}/resource/${item.href}`}>{item.href}</a>
+        <a href={`/read/${id}?id=${item.id}`}>{item.href}</a>
       {/each}
     </div>
   </Drawer.Content>
