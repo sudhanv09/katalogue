0ece482a4993b43fe134416805b4b8522301bc79
sudhanv09 <mystogun125@gmail.com>
2026-01-16 16:19:19 +0800

    fix: image size too large

--- src/routes/read/[id]/+page.svelte
+++ src/routes/read/[id]/+page.svelte
@@ -6,3 +6,26 @@
 <section class="p-16 h-full">
   {@html data.item.chapter.html}
 </section>
+
+<style>
+  section :global(img) {
+    max-width: 100%;
+    max-height: 90vh;
+    height: auto;
+    object-fit: contain;
+    display: block;
+    margin: 0 auto;
+  }
+
+  section :global(svg) {
+    max-width: 100%;
+    max-height: 90vh;
+    height: auto;
+    display: block;
+    margin: 0 auto;
+  }
+
+  section :global(div) {
+    max-width: 100%;
+  }
+</style>
